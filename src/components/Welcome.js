716f69bb4c36b9c6d594d92c31b6b70d7c4a1b62
0siel <93361992+0siel@users.noreply.github.com>
2024-11-06 17:38:13 -0600

    Responsive0

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,58 +1,14 @@
 import React from "react";
+import "./styles.css";
 
 export default function Welcome() {
   return (
-    <div
-      className="home-section"
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "700px",
-        backgroundColor: "#43523A",
-        margin: "0 8px 0 8px",
-        borderRadius: "3px",
-        paddingLeft: "5px",
-        paddingRight: "5px",
-        width: "auto",
-      }}
-    >
+    <div className="welcome">
       <div style={{ width: "70%" }}>
-        <h2
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: "3em",
-            color: "#EBEFF0",
-          }}
-        >
-          San Felipe
-        </h2>
-        <p
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: "1.5em",
-            color: "#EBEFF0",
-          }}
-        >
-          Tenemos una habitación para ti
-        </p>
+        <h2>San Felipe</h2>
+        <p>Tenemos una habitación para ti</p>
       </div>
-      <img
-        src={`${process.env.PUBLIC_URL}/HF VERDE-PNG.png`}
-        alt="Hotel"
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "contain",
-        }}
-      />
+      <img src={`${process.env.PUBLIC_URL}/HF VERDE-PNG.png`} alt="Hotel" />
     </div>
   );
 }
